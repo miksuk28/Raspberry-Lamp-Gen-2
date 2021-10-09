7808e8e846f70d4c49140fa4d28460b43fa8aef4
Mikhail <miksuk28@gmail.com>
2021-10-09 16:19:04 +0200

    basic test Flask app

---
+++ lamp_service.py
@@ -0,0 +1,31 @@
+from flask import Flask, json, request, jsonify, abort
+
+pins = {"r": 17, "g": 22, "b": 24, "btn": 27}
+
+def setup():
+    import pigpio
+    pi = pigpio.pi()
+
+    return pi
+
+def _set_led(r, g, b):
+    pi.set_PWM_dutycycle(pins["r"], r)
+    pi.set_PWM_dutycycle(pins["g"], g)
+    pi.set_PWM_dutycycle(pins["b"], b)
+
+app = Flask(__name__)
+
+@app.route("/set_led", methods=["POST"])
+def set_led():
+    if request.method == "POST":
+        data = request.get_json()
+        _set_led(data["red"], data["green"], data["blue"])
+
+        return jsonify({"message": "LEDs changed"})
+    else:
+        abort(405)
+
+
+if __name__ == "__main__":
+    pi = setup()
+    app.run(debug=True, host="0.0.0.0", port=6969)
